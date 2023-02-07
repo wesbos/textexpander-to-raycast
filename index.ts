f261843fd74b2f78a581af34c8da6f7a0e451862
Wes Bos <wesbos@gmail.com>
2023-02-07 10:44:09 -0500

    sweet

---
+++ index.ts
@@ -0,0 +1,53 @@
+import fg from 'npm:fast-glob';
+import xml2js from 'npm:xml2js';
+
+const files = await fg('**/group_*.xml');
+
+console.log(`Found ${files.length} TextExpander Snippet Group Files!`);
+
+type TextExpanderSnippet = {
+  key: string[];
+  string: [expansion: string, description: string, snippetValue: string]
+}
+
+type RaycastSnippet = {
+  name: string;
+  text: string;
+  keyword?: string;
+}
+
+function convertToRaycast(snippet: TextExpanderSnippet): RaycastSnippet | undefined {
+
+  const name = snippet.string.at(0);
+  const keyword = snippet.string.at(1);
+  const text = snippet.string.at(2);
+  if(!name || !text) {
+    console.warn('Skipping this snippet because it doesnt have a name and text', { name, text });
+    return;
+  }
+  console.log('Converting', snippet.string);
+  return {
+    // Expansion → Name
+    name,
+    // Description → Keyword
+    keyword,
+    // Snippet Value → Text
+    text,
+  }
+}
+
+// loop over each file, read the contents, and convert to json
+const groups = await Promise.all(files.map(async (file) => {
+  const xml = await Deno.readTextFile(file);
+  const json = await xml2js.parseStringPromise(xml, { explicitArray: false });
+
+  return json.plist.dict.array.at(0).dict as TextExpanderSnippet[];
+}))
+
+const snippets = groups.flat().filter(Boolean);
+
+const raycastSnippets = snippets.map(convertToRaycast).filter(Boolean);
+
+console.log(`Converted ${snippets.length} Snippets to Raycast`);
+await Deno.writeFile('snippets.json', new TextEncoder().encode(JSON.stringify(raycastSnippets, null, 2)));
+console.log(`Your file now lives at ${Deno.cwd()}/snippets.json. Go ahead and import it into Deno`);
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Text Expander → Raycast Snippet Script
+
+Generates a importable snippets.json file from your text expander backup.
+
+1. Go to the folder where your latest Text Expander backups live.
+
+For example:
+
+`cd ~/Library/Application\ Support/TextExpander/Backups/TextExpander\ Backup\ 2023-02-07-0830432_122.textexpanderbackup/`
+
+You can generate this folder by hittingt he "backup now" button in text expander.
+
+2. Run the script.
+
+deno run
